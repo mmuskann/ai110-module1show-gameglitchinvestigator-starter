e0075fa20ccb81ace6b2c8a90a1a8bc683bb7f9b
mmuskann <mmuskann2004@gmail.com>
2026-03-14 18:41:11 -0400

    Move checked guess from app.py to logic and fixed the logic error for check guess

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import random
 import streamlit as st
+from logic_utils import check_guess
 
 def get_range_for_difficulty(difficulty: str):
     if difficulty == "Easy":
@@ -28,23 +29,6 @@ def parse_guess(raw: str):
 
     return True, value, None
 
-
-def check_guess(guess, secret):
-    if guess == secret:
-        return "Win", "🎉 Correct!"
-
-    try:
-        if guess > secret:
-            return "Too High", "📈 Go HIGHER!" # FIXME: Logic breaks here
-        else:
-            return "Too Low", "📉 Go LOWER!" # FIXME: Logic breaks here
-    except TypeError:
-        g = str(guess)
-        if g == secret:
-            return "Win", "🎉 Correct!"
-        if g > secret:
-            return "Too High", "📈 Go HIGHER!"
-        return "Too Low", "📉 Go LOWER!"
 
 
 def update_score(current_score: int, outcome: str, attempt_number: int):
